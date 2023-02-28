9dd6b6911cdcb9df54b424a5bd0fa1c94cb50643
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-02-28 16:22:47 -0300

    Test pass: doorCanBeFlipped

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -1,12 +1,16 @@
 public class DoorsFlipper {
+    private boolean doors[];
+
     public DoorsFlipper(int i) {
+        doors = new boolean[1];
+        doors[0] = false;
     }
 
     public boolean isClosed(int i) {
-        return true;
+        return !doors[0];
     }
 
     public void flipEvery(int i) {
-        throw new RuntimeException();
+        doors[0] = true;
     }
 }
