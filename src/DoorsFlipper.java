cbe6cfdcf07a645a892121a50f856f84ed9b0059
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-15 11:19:37 -0300

    Refactoring: Renombrear doors a opened. Borrar closeAllDoors

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -3,51 +3,45 @@ public class DoorsFlipper {
     public static final String INVALID_FLIP_STEP = "Flip steep must be between 1 and numbers of doors";
     public static final String INVALID_DOOR_POSITION = "Door position must be between 0 and number of door - 1";
 
-    private boolean doors[];
+    private boolean[] opened;
 
     public DoorsFlipper(int numberOfDoors) {
         assertValidNumberOfDoors(numberOfDoors);
 
-        doors = new boolean[numberOfDoors];
-        closeAllDoors();
+        opened = new boolean[numberOfDoors];
     }
 
     private void assertValidNumberOfDoors(int numberOfDoors) {
         if (numberOfDoors < 1) throw new IllegalArgumentException(INVALID_NUMBER_OF_DOORS);
     }
 
-    private void closeAllDoors() {
-        for (int doorIndex = 0; doorIndex < doors.length; doorIndex++)
-            doors[doorIndex] = false;
-    }
-
     public boolean isClosed(int doorPosition) {
         assertValidDoorPosition(doorPosition);
 
-        return !doors[doorPosition-1];
+        return !opened[doorPosition-1];
     }
 
     private void assertValidDoorPosition(int doorPosition) {
-        if(doorPosition < 1 || doorPosition > doors.length) throw new IllegalArgumentException(INVALID_DOOR_POSITION);
+        if(doorPosition < 1 || doorPosition > opened.length) throw new IllegalArgumentException(INVALID_DOOR_POSITION);
     }
 
     public void flipEvery(int step) {
         assertValidStep(step);
 
-        for (int doorIndex = step-1; doorIndex < doors.length ; doorIndex+=step)
+        for (int doorIndex = step-1; doorIndex < opened.length ; doorIndex+=step)
             flipAt(doorIndex);
     }
 
     private void flipAt(int doorIndex) {
-        doors[doorIndex] = !doors[doorIndex];
+        opened[doorIndex] = !opened[doorIndex];
     }
 
     private void assertValidStep(int step) {
-        if (step < 1 || step > doors.length) throw new IllegalArgumentException(INVALID_FLIP_STEP);
+        if (step < 1 || step > opened.length) throw new IllegalArgumentException(INVALID_FLIP_STEP);
     }
 
     public void flipAll() {
-        for (int step = 1; step <= doors.length; step++)
+        for (int step = 1; step <= opened.length; step++)
             flipEvery(step);
     }
 }
